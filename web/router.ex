6b6fce5732d3fbf94e95fc9d1ea6773e6ca227bd
Anthony McDonald <a@awews.com>
2017-09-24 14:37:11 -0600

    update router from 'json' to 'json-api'

--- web/router.ex
+++ web/router.ex
@@ -2,7 +2,7 @@ defmodule ElixirBlog.Router do
   use ElixirBlog.Web, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug :accepts, ["json-api"]
   end
 
   scope "/api", ElixirBlog do
