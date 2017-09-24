8bff60ba9e34c379e34d79772e1aebf2a27a2259
Anthony McDonald <a@awews.com>
2017-09-24 14:57:41 -0600

    add dummy data to seed file then run 'mix run priv/repo/seeds.exs' to enter it into the database

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,27 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias ElixirBlog.Repo
+alias ElixirBlog.Post
+
+[
+  %Post{
+    title: "Why do I like Elixir",
+    image: "https://avatars0.githubusercontent.com/u/1481354?v=4&s=400",
+    body: "I like Elixir a lot",
+    likes: 5,
+    author: "aweful",
+    category: "software development",
+    sub_category: "elixir"
+  },
+  %Post{
+    title: "Why do I like Vue.JS",
+    image: "http://pespantelis.github.io/vue-videobg/demo/assets/logo.png",
+    body: "I like Vue.JS a lot",
+    likes: 7,
+    author: "aweful",
+    category: "software development",
+    sub_category: "vuejs"
+  }
+] |> Enum.each(&Repo.insert!(&1))
